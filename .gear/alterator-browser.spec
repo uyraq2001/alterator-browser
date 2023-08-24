1cc3e345aa4dd0cd6ea45cff6a54646151cd2fd9
Kozyrev Yuri <kozyrevid@altlinux.org>
2023-08-24 17:40:31 +0400

    chore: changed spec

---
+++ .gear/alterator-browser.spec
@@ -0,0 +1,45 @@
+Name: alterator-browser
+Version: 0.1.0
+Release: alt1
+
+Summary: Revised alterator
+License: GPLv2+
+Group: Other
+URL: https://github.com/uyraq2001/alterator-module-browser
+
+BuildRequires(pre): rpm-macros-cmake
+BuildRequires: cmake
+BuildRequires: gcc-c++
+BuildRequires: qt5-base-devel
+BuildRequires: qt5-tools-devel
+BuildRequires: qt5-base-common
+BuildRequires: boost-devel-headers
+
+Requires: alterator
+Requires: alterator-manager
+Requires: alterator-module-executor
+
+Source0: %name-%version.tar
+
+%description
+Alterator operating via D-Bus.
+
+%prep
+%setup
+
+%build
+%cmake
+%cmake_build
+
+%install
+%cmakeinstall_std
+
+%files
+%_bindir/altcenter
+%doc README.md INSTALL.md
+
+%changelog
+* Wed Aug 1 2023 Kozyrev Yuri <kozyrevid@altlinux.org> 0.1.0-alt1
+- added support for acc files
+* Wed Jul 5 2023 Kozyrev Yuri <kozyrevid@altlinux.org> 0.0.1-alt1
+- initial build
---
+++ cmake/TranslationUtils.cmake
@@ -0,0 +1,30 @@
+function(add_translation_resource out_file prefix)
+    set(qm_files ${ARGN})
+    set(resource ${CMAKE_CURRENT_SOURCE_DIR}/${prefix}.tr.qrc)
+
+    file(WRITE ${resource} "<!DOCTYPE RCC><RCC version=\"1.0\">\n <qresource prefix=\"/\">\n")
+    foreach(LANG ${qm_files})
+        get_filename_component(file_name ${LANG} NAME)
+        file(APPEND ${resource} "  <file>${file_name}</file>\n")
+    endforeach()
+    file(APPEND ${resource} " </qresource>\n</RCC>\n")
+
+    set(${out_file} ${resource} PARENT_SCOPE)
+endfunction()
+
+function(add_translation out_files)
+    set(qm_files "")
+    foreach(filename ${ARGN})
+        get_filename_component(basename ${filename} NAME_WE)
+        set(qm "${CMAKE_CURRENT_SOURCE_DIR}/${basename}.qm")
+        add_custom_command(
+            OUTPUT "${qm}"
+            COMMAND "${Qt5_LRELEASE_EXECUTABLE}"
+            ARGS -markuntranslated "Is not translated!" -nounfinished -removeidentical -compress "${filename}" -qm "${qm}"
+            DEPENDS "${filename}" VERBATIM
+        )
+        list(APPEND qm_files "${qm}")
+    endforeach()
+
+    set(${out_files} ${qm_files} PARENT_SCOPE)
+endfunction()
