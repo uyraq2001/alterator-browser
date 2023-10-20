26a195e393b4ff011425cd57b406f910eecbc39f
Alexey Saprunov <workernight@yandex.ru>
2023-10-20 19:26:16 +0400

    chore: change methods names in category and application object

--- src/app/model/objectsmodelbuilder.cpp
+++ src/app/model/objectsmodelbuilder.cpp
@@ -20,15 +20,15 @@ const QString DBUS_PATH                            = "/ru/basealt/alterator";
 const QString DBUS_FIND_INTERFACE_NAME             = "ru.basealt.alterator.object";
 const QString DBUS_MANAGER_INTERFACE_NAME          = "ru.basealt.alterator.manager";
 const QString GET_OBJECTS_METHOD_NAME              = "GetObjects";
-const QString INFO_METHOD_NAME_FOR_ACOBJECT        = "info";
+const QString INFO_METHOD_NAME_FOR_ACOBJECT        = "Info";
 const QString CATEGORY_INTERFACE_NAME_FOR_ACOBJECT = "ru.basealt.alterator.categories";
 const QString CATEGORY_OBJECT_PATH                 = "/ru/basealt/alterator/categories";
-const QString CATEGORY_METHOD_NAME_FOR_ACOBJECT    = "info";
+const QString CATEGORY_METHOD_NAME_FOR_ACOBJECT    = "Info";
 
 const QString DBUS_LOCAL_APP_PATH              = "/ru/basealt/alterator/applications";
 const QString DBUS_LOCAL_APP_INTERFACE_NAME    = "ru.basealt.alterator.applications";
-const QString DBUS_LOCAL_APP_GET_LIST_OF_FILES = "list";
-const QString DBUS_LOCAL_APP_GET_DESKTOP_FILE  = "info";
+const QString DBUS_LOCAL_APP_GET_LIST_OF_FILES = "List";
+const QString DBUS_LOCAL_APP_GET_DESKTOP_FILE  = "Info";
 
 namespace ab
 {
